0485bc713a7f84c8749c6ddd5d9336fd760d8a5a
Abubeker <Abubekera87@gmail.com>
2024-11-27 17:05:06 +0300

    create a route for deleting a node

---
+++ app/api/node/[nodeId]/route.ts
@@ -0,0 +1,23 @@
+import prisma from "@/prisma/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { nodeId: string } }
+) {
+  const story = await prisma.node.findUnique({
+    where: {
+      id: params.nodeId,
+    },
+  });
+
+  if (!story) return NextResponse.json("Invalid node", { status: 404 });
+
+  await prisma.story.delete({
+    where: {
+      id: params.nodeId,
+    },
+  });
+
+  return NextResponse.json({});
+}
