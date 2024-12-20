a14021adb58d4ac7e63296cc23f2db4f945e446e
Abubeker <Abubekera87@gmail.com>
2024-12-20 15:12:58 +0300

    made the call to action section responsive

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <Wellcome />
       <Features />
       <Testimonial />
-      {/* <CallToAction />
-      <Footer /> */}
+      <CallToAction />
+      {/* <Footer /> */}
     </div>
   );
 }
--- components/landing page/CallToAction.tsx
+++ components/landing page/CallToAction.tsx
@@ -3,9 +3,11 @@ import { Button } from "../ui/button";
 
 const CallToAction = () => {
   return (
-    <div className=" flex flex-col items-center justify-center w-full bg-slate-100 py-10 gap-5">
-      <p className="text-6xl font-bold">Start Your Adventure Today</p>
-      <p className=" text-xl">Join a community of 10,000 storytellers!</p>
+    <div className=" flex flex-col items-center justify-center w-full bg-slate-100 py-5 gap-3 lg:py-10 lg:gap-5">
+      <p className=" ~text-xl/5xl font-bold">Start Your Adventure Today</p>
+      <p className=" ~text-base/3xl font-light">
+        Join a community of 10,000 storytellers!
+      </p>
       <Button className=" rounded-full">Begin Your Journey</Button>
     </div>
   );
