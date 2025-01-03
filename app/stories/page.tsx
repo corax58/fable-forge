a296a5d81d8223e1c741d581350645de053229d4
Abubeker Abduljelil <abubekera87@gmail.com>
2025-01-03 14:23:07 +0300

    Fix the stories page

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -1,7 +1,8 @@
 import StoryCard from "@/components/story/StoryCard";
+import { FetchedStories } from "@/lib/types";
 import prisma from "@/prisma/db";
 
-const BrowseStoriesPage = async () => {
+export async function getServerSideProps() {
   const Stories = await prisma.story.findMany({
     include: {
       author: true,
@@ -9,8 +10,14 @@ const BrowseStoriesPage = async () => {
     },
   });
 
+  return {
+    props: { Stories },
+  };
+}
+
+const BrowseStoriesPage = ({ Stories }: { Stories: FetchedStories[] }) => {
   return (
-    <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full  ">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full">
       {Stories.map((story) => (
         <StoryCard story={story} key={story.id} />
       ))}
