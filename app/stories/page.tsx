b8b9d7598279a743446c676f1f9a3dcd5c608788
Abubeker Abduljelil <114282920+corax58@users.noreply.github.com>
2025-01-17 19:55:17 +0300

    Remove getServerSideProps

--- app/stories/page.tsx
+++ app/stories/page.tsx
@@ -1,8 +1,9 @@
 import StoryCard from "@/components/story/StoryCard";
-import { FetchedStories } from "@/lib/types";
 import prisma from "@/prisma/db";
+import { FetchedStories } from "@/lib/types";
 
-export async function getServerSideProps() {
+// Fetch stories from the database
+async function fetchStories(): Promise<FetchedStories[]> {
   const Stories = await prisma.story.findMany({
     include: {
       author: true,
@@ -10,12 +11,13 @@ export async function getServerSideProps() {
     },
   });
 
-  return {
-    props: { Stories },
-  };
+  return Stories;
 }
 
-const BrowseStoriesPage = ({ Stories }: { Stories: FetchedStories[] }) => {
+// Server component
+const BrowseStoriesPage = async () => {
+  const Stories = await fetchStories();
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full">
       {Stories.map((story) => (
